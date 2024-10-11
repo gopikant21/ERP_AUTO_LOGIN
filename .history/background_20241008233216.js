5d21ac7f54053b5120210a7ee9e0ccf3edc3337a
gopikant21 <gopikant5jan@gmail.com>
2024-10-11 16:15:16 +0530

    history tracking disable

--- .history/background_20241008233216.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(function (details) {
-    if (details.reason === "install") {
-      // Open the login page when the extension is installed
-      chrome.tabs.create({
-        url: chrome.runtime.getURL("erp_page.html")
-      });
-    }
-  });
-  
--- .history/popup_20241010134643.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('activate-button').addEventListener('click', () => {
-    // Send a message to the content script
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, {action: 'activate'});
-    });
-});
